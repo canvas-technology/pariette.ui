2fc1e749217d5919b02d97c389c10811efccecb3
ahmets.cil <ahmets.cil@gmail.com>
2024-03-29 13:42:30 +0300

    build

--- dist/cjs/parietteConfig.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const glob: {
-    site_url: string | undefined;
-    api_url: string | undefined;
-    site_title: string | undefined;
-    company_token: string | undefined;
-    site_token: string | undefined;
-    site_version: string | undefined;
-};
--- dist/esm/src/lib/pariette.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function $get(url: string): Promise<any>;
-export declare function $post(url: string, ctx: object, headers?: object): Promise<any>;
-export declare function $put(url: string, ctx: object, headers?: object): Promise<any>;
-export declare function $delete(url: string): Promise<any>;
